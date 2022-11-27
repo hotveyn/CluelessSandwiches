414c093157be6ccc47d14ca11325f42b16fc682c
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-28 00:31:46 +0300

    Create LayoutDefault.vue

---
+++ src/layouts/LayoutDefault.vue
@@ -0,0 +1,18 @@
+<template>
+  <TheHeader/>
+  <main class="page">
+      <slot/>
+  </main>
+  <TheFooter/>
+</template>
+
+<script setup lang="ts">
+import TheHeader from "@/components/header/TheHeader.vue";
+import TheFooter from "@/components/footer/TheFooter.vue";
+
+</script>
+
+<style scoped lang="scss">
+.page-container{
+}
+</style>
