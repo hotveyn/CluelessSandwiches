eda03cfa984e1be99df69dbb2b296cb0e99c33f3
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-28 00:31:50 +0300

    Create TheFooter.vue

---
+++ src/components/footer/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <footer>
+    <ul>
+      <li
+          v-for="(value,link) in link"
+      >
+        <RouterLink :to="{name:link}">
+          {{ value }}
+        </RouterLink>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<script setup lang="ts">
+
+const link = {home: "Home", catalog: "Catalog"};
+</script>
+
+<style scoped>
+
+</style>
