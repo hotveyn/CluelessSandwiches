dca2d7ee0b165dc1c8bcb8bbf5d900d399855ea6
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-28 15:30:31 +0300

    Create ProfilePage.vue

---
+++ src/pages/ProfilePage.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <main>
+    <!--  Если юзер не авторизован-->
+    <div class="main-container">
+      <LogReg/>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import LogReg from "@/components/profile/LogReg.vue";
+
+</script>
+
+<style scoped lang="scss">
+@import "@/mixin.scss";
+
+main {
+  min-height: 80vh;
+  background: linear-gradient(248.99deg, #38E6EB -37.47%, #2DEB80 132.11%);
+  @include myFlex();
+
+  .main-container{
+    max-width: 1300px;
+    margin: 0 auto;
+  }
+}
+</style>
