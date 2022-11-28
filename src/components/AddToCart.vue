2fe302c6a0a84ff994d91cfa5c3bd03ab4282d26
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-28 15:31:09 +0300

    Create AddToCart.vue

---
+++ src/components/AddToCart.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+      @click="addToCart"
+    class="add-to-cart"
+  >
+    <span class="material-symbols-outlined">
+      add
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+
+function addToCart(){
+
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/mixin.scss";
+
+.add-to-cart{
+  @include myFlex($jc:center);
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  color:black;
+  cursor: pointer;
+  border: white;
+  padding: 0;
+  box-shadow: 0 4px 6px 0 rgb(0 0 0 / 8%);
+}
+</style>
