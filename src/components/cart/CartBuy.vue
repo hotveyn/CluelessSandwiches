74307341c462446e0e87a6b386280a05b0b266eb
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-28 15:31:22 +0300

    Create CartBuy.vue

---
+++ src/components/cart/CartBuy.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="buy">
+    <form action="http://127.0.0.1:8000/api/order/create">
+      <p>Цена всех товаров - 60$</p>
+      <button
+          class="button-buy"
+      >
+        Оформить
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped lang="scss">
+@import "@/mixin.scss";
+
+.buy {
+  max-width: 400px;
+  width: 100%;
+  background-color: white;
+  padding: 30px;
+  font-family: Comfortaa, cursive;
+  font-weight: 600;
+
+  button {
+    margin-top: 30px;
+    min-height: 50px;
+    padding: 10px ;
+    width: 100%;
+    border: none;
+    border-radius: 15px;
+    font-size: 26px;
+    color: white;
+    background: linear-gradient(248.99deg, #38E6EB -37.47%, #2DEB80 132.11%);
+
+  }
+}
+</style>
