b16c393c1eb41e01599f78c9e1ebc68173a6f083
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-28 15:30:27 +0300

    add interfaces

---
+++ src/interfaces/IProduct.ts
@@ -0,0 +1,11 @@
+import ICategory from "@/interfaces/ICategory";
+import IOption from "@/interfaces/IOption";
+
+export default interface IProduct{
+    id: number,
+    name: string,
+    price: number,
+    description: string,
+    category: ICategory,
+    options: IOption
+}
