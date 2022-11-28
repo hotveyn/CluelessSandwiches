86f728de125a2a999ce2c921f8526909d81426a1
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-28 15:30:38 +0300

    Update router.ts

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,9 @@
 import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
 import HomePage from "@/pages/HomePage.vue"
 import CatalogPage from "@/pages/CatalogPage.vue"
+import ProfilePage from "@/pages/ProfilePage.vue"
+import CartPage from "@/pages/CartPage.vue"
+import ProductPage from "@/pages/ProductPage.vue"
 
 const routes: RouteRecordRaw[] = [
     {
@@ -13,6 +16,21 @@ const routes: RouteRecordRaw[] = [
         name: "catalog",
         component: CatalogPage
     },
+    {
+        path: "/profile",
+        name: "profile",
+        component: ProfilePage
+    },
+    {
+        path: "/cart",
+        name: "cart",
+        component: CartPage
+    },
+    {
+        path: "/product/:id",
+        name: "product",
+        component: ProductPage
+    }
 ]
 
 export default createRouter({
