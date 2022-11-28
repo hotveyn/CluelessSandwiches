dd744525fe9bd94fa30556d1320243fc5b7139e8
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-28 15:31:27 +0300

    Create CartItemCounter.vue

---
+++ src/components/cart/CartItemCounter.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="counter-button">
+    <button class="plus">
+    <span class="material-symbols-outlined">
+      add
+    </span>
+    </button>
+    <p>1</p>
+    <button class="minus">
+    <span class="material-symbols-outlined">
+      remove
+    </span>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+@import "@/mixin.scss";
+
+.counter-button {
+  @include myFlex($jc: center);
+  transform: scale(1.2, 1.2);
+  box-sizing: border-box;
+  width: 90px;
+  overflow: hidden;
+  border: none;
+  border-radius: 15px;
+
+  button, p {
+    width: 30px;
+    @include myFlex($jc: center);
+  }
+
+  button {
+    background: linear-gradient(248.99deg, #38E6EB -37.47%, #2DEB80 132.11%);
+    border: none;
+    cursor: pointer;
+    color: white;
+  }
+
+  p {
+    font-family: Comfortaa, cursive;
+  }
+}
+</style>
