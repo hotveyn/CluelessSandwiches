91bfd3a965fe601da9376ae47c4f295e9bdc4a9b
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-29 18:14:30 +0300

    Create TheSections.vue

---
+++ src/components/main/TheSections.vue
@@ -0,0 +1,32 @@
+<template>
+  <SectionItem
+      v-for="(category, index) in categories"
+      :key="index"
+      :category-name="category.name"
+      :category-id="category.id"
+  />
+</template>
+
+<script setup lang="ts">
+import SectionItem from "@/components/main/SectionItem.vue"
+
+
+import {onMounted, ref} from "vue";
+
+const categories = ref({});
+
+async function getCategories() {
+  fetch("http://127.0.0.1:8000/api/categories/")
+      .then(response => response.json())
+      .then(data => {
+        categories.value = data;
+      });
+}
+onMounted(()=>{
+  getCategories()
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
